72cc906889a6cb72abd1fc2e85999f7f912f1fb3
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2022-02-26 20:15:47 +0600

    Create Find Minimum in Rotated Sorted Array if contain duplicated.cpp

---
+++ Binary Search/Find Minimum in Rotated Sorted Array if contain duplicated.cpp
@@ -0,0 +1,22 @@
+int findMin(vector<int>& nums) {
+        
+        int low = 0, high = nums.size()-1, mid=0;
+        
+        while(low<high){
+            mid = low + (high-low)/2;
+            
+            if(nums[mid] > nums[high]) low = mid+1;
+            else if(nums[mid] < nums[high]) high = mid;
+            // When the minimum number found
+            else {
+                if(nums[high-1] > nums[high]){
+                    low = high;
+                    break;
+                }
+                else high--;
+                
+            }
+        }
+        
+        return nums[low];
+}
